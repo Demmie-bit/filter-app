9844d3fd6bc74fe6a3383b459fa24bd1f7e16fca
Demmie-bit <77945051+Demmie-bit@users.noreply.github.com>
2022-03-19 11:59:13 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function preload(){
-
+    lip_img = loadImage('https://i.postimg.cc/sg2p0k0S/PNGPIX-COM-Lips-PNG-Transparent-Image-1-500x313.png');
 }
 
 function setup(){
@@ -11,8 +11,6 @@ function setup(){
 
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on("pose", gotPoses);
-
-    circle(noseX, noseY, 20);
 }
 function modelLoaded(){
     console.log("PoseNet is here UwU.");
@@ -20,19 +18,20 @@ function modelLoaded(){
 function gotPoses(results){
     if (results.length > 0) {
        console.log(results);
-       noseX = results[0].pose.nose.x;
-       noseY = results[0].pose.nose.y;
+       lipX = results[0].pose.nose.x - 25;
+       lipY = results[0].pose.nose.y + 15;
     }
 };
 
 function draw(){
     image(video, 0, 0, 400, 300);
-    circle(noseX, noseY, 20);
+    image(lip_img, lipX, lipY, 50, 20);
+    
     
 }
 
 function take_snapshot(){
     save('Picture.png');
 }
-var noseX = 0;
-var noseY = 0;
+lipX = 0;
+lipY = 0;
